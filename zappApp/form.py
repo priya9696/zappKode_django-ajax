8afd8a4e11d619100cbd25e3cb12063002c3616c
Priyanka Deepak Walnekar <54662537+priya9696@users.noreply.github.com>
2021-07-01 20:26:33 +0530

    Add files via upload

---
+++ zappApp/form.py
@@ -0,0 +1,20 @@
+from django import forms 
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(forms.Form):
+    name= forms.CharField(max_length=100, required= True)
+    username= forms.CharField(max_length=100, required= True)
+    email= forms.CharField(max_length=100, required= True)
+    contact= forms.IntegerField(required= True)
+    password= forms.CharField(max_length=20, required= True, widget= forms.PasswordInput)
+    confirm_password= forms.CharField(max_length=20, required= True, widget=forms.PasswordInput )
+
+
+class UserLoginForm(forms.Form):
+    username= forms.CharField()
+    password= forms.CharField(widget= forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
